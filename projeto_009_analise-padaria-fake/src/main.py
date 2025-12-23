4f11c461ccb2db28642b3e12acbca5d5b66a8726
William Teixeira <smooth.poa@gmail.com>
2025-12-23 18:55:38 -0300

    feat(analise-padaria-fake): adiciona análise faturamento diário

--- projeto_009_analise-padaria-fake/src/main.py
+++ projeto_009_analise-padaria-fake/src/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 
 CSV_PATH = "data/vendas_padaria.csv"
 
@@ -23,11 +24,41 @@ def main():
     print("Dataset carregado com sucesso")
     print(f"Linhas: {df.shape[0]} | Colunas: {df.shape[1]}")
 
-    print("\nColunas:")
-    print(df.columns.tolist())
+    # ------------------------- #
+    # Preparação
+    # ------------------------- #
+    df["data"] = pd.to_datetime(df["data"], errors="coerce")
+    df["total_venda"] = df["quantidade"] * df["valor_unitario"]
 
-    print("\nAmostra dos dados:")
-    print(df.head())
+    # ------------------------- #
+    # Pergunta:
+    # Qual foi o total de vendas por dia?
+    # ------------------------- #
+    vendas_por_dia = (
+        df.groupby("data")["total_venda"]
+          .sum()
+          .reset_index()
+          .sort_values("data")
+    )
+
+    print("\nTotal de vendas por dia:")
+    print(vendas_por_dia)
+
+    # ------------------------- #
+    # Visualização simples
+    # ------------------------- #
+    fig = px.line(
+        vendas_por_dia,
+        x="data",
+        y="total_venda",
+        title="Faturamento Diário da Padaria",
+        labels={
+            "data": "Data",
+            "total_venda": "Faturamento (R$)"
+        }
+    )
+
+    fig.show()
 
 if __name__ == "__main__":
     main()
